b9c9905a4d8400a38b6c082e253db2140bcbaeea
abitha000 <abithak000@yahoo.com>
2026-08-27 11:53:09 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -148,7 +148,7 @@ REDIS_DB = KInit.REDIS_DB
 
 # Configure Redis connection
 redis_conn = Redis(
-    host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, db=REDIS_DB
+    host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, db=REDIS_DB, ssl=True
 )
 
 # Test Redis connection
